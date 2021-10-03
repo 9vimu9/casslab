42070d52e1d400c1266d1496f0c59dfad5d486db
vimukthi <vimukthi@efuturesworld.com>
2021-10-03 17:44:53 +0530

    refactor prediction.dart

--- lib/screens/Prediction/components/body.dart
+++ lib/screens/Prediction/components/body.dart
@@ -95,164 +95,212 @@ class _BodyState extends State<Body> {
   Widget build(BuildContext context) {
     size = MediaQuery.of(context).size;
     return Background(
-      child: SingleChildScrollView(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            TopButtonBar(),
-            imagePreviewWidget(),
-            Row(children: <Widget>[
-              SizedBox(width: size.width * 0.1),
-              SizedBox(
-                  width: size.width * 0.2,
-                  child: ElevatedButton(
-                    onPressed: pickImage,
-                    child: const Icon(Icons.camera_alt, size: 35),
-                    style: ButtonStyle(
-                      shape: MaterialStateProperty.all(CircleBorder()),
-                      padding: MaterialStateProperty.all(EdgeInsets.all(10)),
-                      backgroundColor: MaterialStateProperty.all(
-                          kPrimaryLightColor), // <-- Button color
-                      overlayColor:
-                          MaterialStateProperty.resolveWith<Color?>((states) {
-                        if (states.contains(MaterialState.pressed))
-                          return kPrimaryCancelColor; // <-- Splash color
-                      }),
-                    ),
-                  )),
-              SizedBox(width: size.width * 0.1),
-              SizedBox(
-                  width: size.width * 0.5,
-                  child: RoundedButton(
-                    text: "Pick From Gallery",
-                    color: kPrimaryLightColor,
-                    textColor: Colors.black,
-                    press: pickGalleryImage,
-                    width: size.width * 0.5,
-                  )),
-              SizedBox(width: size.width * 0.1),
-            ]),
-            RoundedButton(
-              text: "My Predictions",
-              color: kPrimaryLightColor,
-              textColor: Colors.black,
-              press: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) {
-                      return ListScreen();
-                    },
-                  ),
-                );
-              },
-            ),
-          ],
-        ),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          TopButtonBar(),
+          predictionWidget(),
+          actionButtonsBar(),
+        ],
       ),
     );
   }
 
-  Widget imagePreviewWidget() {
+  Widget actionButtonsBar() {
+    return Column(
+      children: [
+        Row(children: <Widget>[
+          SizedBox(width: size.width * 0.1),
+          SizedBox(
+              width: size.width * 0.2,
+              child: ElevatedButton(
+                onPressed: pickImage,
+                child: const Icon(Icons.camera_alt, size: 35),
+                style: ButtonStyle(
+                  shape: MaterialStateProperty.all(CircleBorder()),
+                  padding: MaterialStateProperty.all(EdgeInsets.all(10)),
+                  backgroundColor: MaterialStateProperty.all(
+                      kPrimaryLightColor), // <-- Button color
+                  overlayColor:
+                  MaterialStateProperty.resolveWith<Color?>((states) {
+                    if (states.contains(MaterialState.pressed))
+                      return kPrimaryCancelColor; // <-- Splash color
+                  }),
+                ),
+              )),
+          SizedBox(width: size.width * 0.1),
+          SizedBox(
+              width: size.width * 0.5,
+              child: RoundedButton(
+                text: "Pick From Gallery",
+                color: kPrimaryLightColor,
+                textColor: Colors.black,
+                press: pickGalleryImage,
+                width: size.width * 0.5,
+              )),
+          SizedBox(width: size.width * 0.1),
+        ]),
+        RoundedButton(
+          text: "My Predictions",
+          color: kPrimaryLightColor,
+          textColor: Colors.black,
+          press: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) {
+                  return ListScreen();
+                },
+              ),
+            );
+          },
+        ),
+      ],
+      mainAxisAlignment: MainAxisAlignment.center,
+    );
+  }
+
+  Widget predictionWidget() {
     //show nothing if no picture selected
     if (_noImageSelected || _image == null) {
       return Container(
-        height: size.height * 0.6,
+        height: size.height * 0.65,
       );
     }
 
     return Center(
       child: Column(
         children: [
-          Container(
-            height: size.height * 0.6,
-            child: OverflowBox(
-                minWidth: 0.0,
-                minHeight: 0.0,
-                maxWidth: double.infinity,
-                child: Image.file(
-                  _image!,
-                  fit: BoxFit.cover,
-                )),
-          ),
-          const SizedBox(
-            height: 5,
-          ),
-          Row(children: <Widget>[
-            SizedBox(width: size.width * 0.1),
-            SizedBox(
-              width: size.width * 0.6,
-              child: Text(
-                '${_output[0]['label']}',
-                textAlign: TextAlign.left,
-                style: const TextStyle(
-                    color: Colors.black,
-                    fontSize: 35,
-                    fontWeight: FontWeight.w600),
-              ),
-            ),
-            SizedBox(width: size.width * 0.1),
-            SizedBox(
-                width: size.width * 0.1,
-                child: IconButton(
-                  icon: Icon(
-                      _addedToFavourite
-                          ? Icons.favorite_outlined
-                          : Icons.favorite_border,
-                      size: 36,
-                      color: kPrimaryColor),
-                  onPressed: () {
-                    if (_addedToFavourite) {
-                      // remove from favourite list
-                      // ask question before do that
-
-                      var result = showDialog<String>(
-                        context: context,
-                        builder: (BuildContext context) => AlertDialog(
-                          title: const Text('Removing from favourites'),
-                          content: const Text('Do you really want to this remove from favourites ?'),
-                          elevation: 24,
-                          actions: <Widget>[
-                            TextButton(
-                              onPressed: () => Navigator.pop(context, favouritesDoNotRemove),
-                              child: const Text('No, Keep it'),
-                            ),
-                            TextButton(
-                              onPressed: () => Navigator.pop(context, favouritesRemove),
-                              child: const Text('Yes, Remove it'),
-                            ),
-                          ],
-                        ),
-                      );
-                      result.then((userDecision) {
-                        if(userDecision == favouritesRemove){
-                          setState(() {
-                            _addedToFavourite = false;
-                          });
-                        }
-                      });
-
-
-
-                    } else {
-                      AddToFavoritesDialog(
-                        _description,
-                        context,
-                        _formKey,
-                        onSaveFavourite,
-                        onCancelFavouriteDialog,
-                      ).displayAddToFavoritesDialog();
-                    }
-                  },
-                )),
-            SizedBox(width: size.width * 0.1),
-          ]),
-          const SizedBox(
-            height: 10,
-          ),
+          descriptionBar(),
+          imagePreview(),
+          predictionLabelBar(),
         ],
       ),
     );
+  }
+
+  Widget descriptionBar() {
+    return _addedToFavourite
+        ? Row(
+            children: <Widget>[
+              SizedBox(width: size.width * 0.05),
+              SizedBox(
+                width: size.width * 0.8,
+                child: Container(
+                    child: Text(
+                  '${_description}',
+                  textAlign: TextAlign.left,
+                  style: const TextStyle(
+                      color: Colors.black,
+                      fontSize: 20,
+                      fontWeight: FontWeight.w600),
+                )),
+              ),
+              SizedBox(
+                width: size.width * 0.1,
+                child: IconButton(
+                  icon: const Icon(Icons.edit, color: kPrimaryColor),
+                  onPressed: () {
+                    AddToFavoritesDialog(
+                      _description,
+                      context,
+                      _formKey,
+                      onSaveFavourite,
+                      onCancelFavouriteDialog,
+                    ).displayAddToFavoritesDialog();
+                  },
+                ),
+              ),
+              SizedBox(
+                width: size.width * 0.05,
+              ),
+            ],
+          )
+        : Container();
+  }
+
+  Widget imagePreview() {
+    return Container(
+      height: size.height * 0.55,
+      child: OverflowBox(
+          minWidth: 0.0,
+          minHeight: 0.0,
+          maxWidth: double.infinity,
+          child: Image.file(
+            _image!,
+            fit: BoxFit.cover,
+          )),
+    );
+  }
+
+  Widget predictionLabelBar() {
+    return Row(children: <Widget>[
+      SizedBox(width: size.width * 0.1),
+      SizedBox(
+        width: size.width * 0.6,
+        child: Text(
+          '${_output[0]['label']}',
+          textAlign: TextAlign.left,
+          style: const TextStyle(
+              color: Colors.black, fontSize: 35, fontWeight: FontWeight.w600),
+        ),
+      ),
+      SizedBox(width: size.width * 0.1),
+      SizedBox(
+          width: size.width * 0.1,
+          child: IconButton(
+            icon: Icon(
+                _addedToFavourite
+                    ? Icons.favorite_outlined
+                    : Icons.favorite_border,
+                size: 36,
+                color: kPrimaryColor),
+            onPressed: () {
+              if (_addedToFavourite) {
+                // remove from favourite list
+                // ask question before do that
+
+                var result = showDialog<String>(
+                  context: context,
+                  builder: (BuildContext context) => AlertDialog(
+                    title: const Text('Removing from favourites'),
+                    content: const Text(
+                        'Do you really want to this remove from favourites ?'),
+                    elevation: 24,
+                    actions: <Widget>[
+                      TextButton(
+                        onPressed: () =>
+                            Navigator.pop(context, favouritesDoNotRemove),
+                        child: const Text('No, Keep it'),
+                      ),
+                      TextButton(
+                        onPressed: () =>
+                            Navigator.pop(context, favouritesRemove),
+                        child: const Text('Yes, Remove it'),
+                      ),
+                    ],
+                  ),
+                );
+                result.then((userDecision) {
+                  if (userDecision == favouritesRemove) {
+                    setState(() {
+                      _addedToFavourite = false;
+                    });
+                  }
+                });
+              } else {
+                AddToFavoritesDialog(
+                  _description,
+                  context,
+                  _formKey,
+                  onSaveFavourite,
+                  onCancelFavouriteDialog,
+                ).displayAddToFavoritesDialog();
+              }
+            },
+          )),
+      SizedBox(width: size.width * 0.1),
+    ]);
   }
 
   void onSaveFavourite() {
