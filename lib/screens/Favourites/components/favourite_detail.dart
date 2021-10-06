beb0d4ac2e7507e66933233a8b0f61bb5d9230f9
vimukthi <vimukthi@efuturesworld.com>
2021-10-06 16:58:58 +0530

    refactor favourite dertail

--- lib/screens/Favourites/components/favourite_detail.dart
+++ lib/screens/Favourites/components/favourite_detail.dart
@@ -1,11 +1,13 @@
 import 'dart:io';
 
 import 'package:casslab/Model/favourite.dart';
+import 'package:casslab/actions/Favourites/favourites_repository.dart';
 import 'package:casslab/constants.dart';
 import 'package:flutter/material.dart';
 
 class FavouriteDetail extends StatefulWidget {
   Favourite favourite;
+
   FavouriteDetail(this.favourite, {Key? key}) : super(key: key);
 
   @override
@@ -15,6 +17,7 @@ class FavouriteDetail extends StatefulWidget {
 class _FavouriteDetailState extends State<FavouriteDetail> {
   Size? size;
   Favourite favourite;
+
   _FavouriteDetailState(this.favourite);
 
   late String _description;
@@ -24,13 +27,14 @@ class _FavouriteDetailState extends State<FavouriteDetail> {
 
   @override
   void initState() {
-
     _description = favourite.description;
     _prediction = favourite.prediction;
     _id = favourite.id;
-    _dateTaken = DateTime.fromMillisecondsSinceEpoch(favourite.dateTaken).toString();
+    _dateTaken =
+        DateTime.fromMillisecondsSinceEpoch(favourite.dateTaken).toString();
     super.initState();
   }
+
   @override
   Widget build(BuildContext context) {
     size = MediaQuery.of(context).size;
@@ -59,7 +63,8 @@ class _FavouriteDetailState extends State<FavouriteDetail> {
           SizedBox(
             width: size!.width * 0.8,
             child: Container(
-                child: Text(_description ,
+                child: Text(
+              _description,
               textAlign: TextAlign.left,
               style: const TextStyle(
                   color: Colors.black,
@@ -107,74 +112,84 @@ class _FavouriteDetailState extends State<FavouriteDetail> {
           minWidth: 0.0,
           minHeight: 0.0,
           maxWidth: double.infinity,
-          child: _getImagePreview()
-      ),
+          child: _getImagePreview()),
     );
   }
 
   Widget predictionLabelBar() {
-    return Container(child: Row(
-      children: <Widget>[
-        SizedBox(width: size!.width * 0.05),
-        SizedBox(
-          width: size!.width * 0.6,
-          child: Text(
-            _prediction ,
-            textAlign: TextAlign.left,
-            style: const TextStyle(
-                color: Colors.black, fontSize: 35, fontWeight: FontWeight.w600),
+    return Container(
+      child: Row(
+        children: <Widget>[
+          SizedBox(width: size!.width * 0.05),
+          SizedBox(
+            width: size!.width * 0.6,
+            child: Text(
+              _prediction,
+              textAlign: TextAlign.left,
+              style: const TextStyle(
+                  color: Colors.black,
+                  fontSize: 35,
+                  fontWeight: FontWeight.w600),
+            ),
           ),
-        ),
-        SizedBox(width: size!.width * 0.15),
-        SizedBox(
-            width: size!.width * 0.1,
-            child: IconButton(
-              icon: const Icon(Icons.favorite_outlined,
-                  size: 36, color: kPrimaryColor),
-              onPressed: () async {
-                var result = showDialog<String>(
-                  context: context,
-                  builder: (BuildContext context) => AlertDialog(
-                    title: const Text('Removing from favourites'),
-                    content: const Text(
-                        'Do you really want to this remove from favourites ?'),
-                    elevation: 24,
-                    actions: <Widget>[
-                      TextButton(
-                        onPressed: () =>
-                            Navigator.pop(context, favouritesDoNotRemove),
-                        child: const Text('No, Keep it'),
-                      ),
-                      TextButton(
-                        onPressed: () =>
-                            Navigator.pop(context, favouritesRemove),
-                        child: const Text('Yes, Remove it'),
-                      ),
-                    ],
-                  ),
-                );
-                result.then((userDecision) async {
-                  if (userDecision == favouritesRemove) {
-                  }
-                });
-              },
-            )),
-        SizedBox(width: size!.width * 0.05),
-      ],
-    ),padding: EdgeInsets.only(top: 0),);
+          SizedBox(width: size!.width * 0.15),
+          SizedBox(
+              width: size!.width * 0.1,
+              child: IconButton(
+                icon: const Icon(
+                  Icons.delete,
+                  size: 36,
+                  color: kPrimaryColor,
+                ),
+                onPressed: () async {
+                  var result = showDialog<String>(
+                    context: context,
+                    builder: (BuildContext context) => AlertDialog(
+                      title: const Text('Removing from favourites'),
+                      content: const Text(
+                          'Do you really want to this remove from favourites ?'),
+                      elevation: 24,
+                      actions: <Widget>[
+                        TextButton(
+                          onPressed: () =>
+                              Navigator.pop(context, favouritesDoNotRemove),
+                          child: const Text('No, Keep it'),
+                        ),
+                        TextButton(
+                          onPressed: () async {
+                            await FavouritesRepository()
+                                .removeSelectedByFavouriteID(_id);
+                            Navigator.pop(context, favouritesRemove);
+                          },
+                          child: const Text('Yes, Remove it'),
+                        ),
+                      ],
+                    ),
+                  );
+                  result.then((userDecision) async {
+                    if (userDecision == favouritesRemove) {}
+                  });
+                },
+              )),
+          SizedBox(width: size!.width * 0.05),
+        ],
+      ),
+      padding: EdgeInsets.only(top: 0),
+    );
   }
 
-  Image _getImagePreview(){
-
-    if(favourite.imageType == ImageTypes.localStorage){
+  Image _getImagePreview() {
+    if (favourite.imageType == ImageTypes.localStorage) {
       return Image.file(
         //to do set default image if image not available
         File(favourite.imagePath!),
         fit: BoxFit.cover,
       );
-    }else{
-      return Image.network(favourite.imagePath!,fit: BoxFit.cover,);
+    } else {
+      return Image.network(
+        favourite.imagePath!,
+        fit: BoxFit.cover,
+      );
     }
-
   }
 }
